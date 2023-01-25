cee7d31acae634ef628cce5d6236a4f565a98ed6
kzw3933 <3099097649@qq.com>
2023-01-25 17:17:50 +0800

    完成lib-os

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,3 +1,53 @@
-fn main() {
+#![no_main]
+#![no_std]
+#![feature(panic_info_message)]
+
+use core::arch::global_asm;
+#[macro_use]
+mod console;
+mod lang_items;
+mod sbi;
+
+global_asm!(include_str!("entry.asm"));
+
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+
+    extern "C" {
+        fn stext(); // begin addr of text segment
+        fn etext(); // end addr of text segment
+        fn srodata(); // start addr of Read-Only data segment
+        fn erodata(); // end addr of Read-Only data ssegment
+        fn sdata(); // start addr of data segment
+        fn edata(); // end addr of data segment
+        fn sbss(); // start addr of BSS segment
+        fn ebss(); // end addr of BSS segment
+        fn boot_stack_lower_bound(); // stack lower bound
+        fn boot_stack_top(); // stack top
+    }
+
+    clear_bss();
     println!("Hello, world!");
+    println!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
+    println!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
+    println!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
+    println!(
+        "boot_stack_lower_bound={:#x}, boot_stack_top={:#x}",
+        boot_stack_lower_bound as usize, boot_stack_top as usize
+    );
+    println!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
+    panic!("Shutdown machine!");
+}
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| {
+        unsafe {
+            (a as *mut u8).write_volatile(0)
+        }
+    })
 }
