a62504f05a2e9bb0b550f2d7e5f06134ff2d1fd0
kzw3933 <3099097649@qq.com>
2023-01-27 13:37:03 +0800

    完成batch-os

--- os/src/main.rs
+++ os/src/main.rs
@@ -5,49 +5,36 @@
 use core::arch::global_asm;
 #[macro_use]
 mod console;
+pub mod batch;
 mod lang_items;
 mod sbi;
+mod sync;
+pub mod syscall;
+pub mod trap;
 
 global_asm!(include_str!("entry.asm"));
-
-
-#[no_mangle]
-pub fn rust_main() -> ! {
-
-    extern "C" {
-        fn stext(); // begin addr of text segment
-        fn etext(); // end addr of text segment
-        fn srodata(); // start addr of Read-Only data segment
-        fn erodata(); // end addr of Read-Only data ssegment
-        fn sdata(); // start addr of data segment
-        fn edata(); // end addr of data segment
-        fn sbss(); // start addr of BSS segment
-        fn ebss(); // end addr of BSS segment
-        fn boot_stack_lower_bound(); // stack lower bound
-        fn boot_stack_top(); // stack top
-    }
-
-    clear_bss();
-    println!("Hello, world!");
-    println!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
-    println!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
-    println!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
-    println!(
-        "boot_stack_lower_bound={:#x}, boot_stack_top={:#x}",
-        boot_stack_lower_bound as usize, boot_stack_top as usize
-    );
-    println!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
-    panic!("Shutdown machine!");
-}
+global_asm!(include_str!("link_app.S"));
 
 fn clear_bss() {
     extern "C" {
         fn sbss();
         fn ebss();
     }
-    (sbss as usize..ebss as usize).for_each(|a| {
-        unsafe {
-            (a as *mut u8).write_volatile(0)
-        }
-    })
+    unsafe {
+        core::slice::from_raw_parts_mut(sbss as usize as *mut u8, ebss as usize - sbss as usize)
+            .fill(0);
+
+    }
 }
+
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+
+    clear_bss();
+    println!("[kernel] Hello, world!");
+    trap::init();
+    batch::init();
+    batch::run_next_app();
+}
+
